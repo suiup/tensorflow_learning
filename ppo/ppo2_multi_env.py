22c50aa49a74481195618e1b5ec7fe68b2881df5
suiup <931857499@qq.com>
2021-06-07 15:10:11 +0100

    demo multi env

--- ppo/ppo2_multi_env.py
+++ ppo/ppo2_multi_env.py
@@ -1,4 +1,4 @@
-
+import gym
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common import make_vec_env
 from stable_baselines import PPO2
@@ -6,6 +6,8 @@ import numpy as np
 from stable_baselines.common.cmd_util import make_vec_env
 import time
 from env import taxi_env
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines.common import set_global_seeds
 
 
 def evaluate_multi_processes(model, num_steps=1000):
@@ -41,6 +43,23 @@ def evaluate_multi_processes(model, num_steps=1000):
         print("Mean reward:", mean_reward, "Num episodes:", n_episodes)
         return mean_reward
 
+def make_env(env, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
+
+    :param env_id: (str) the environment ID
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+
+    def _init():
+        # Important: use a different seed for each environment
+        env.seed(seed + rank)
+        return env
+
+    set_global_seeds(seed)
+    return _init
+
 
 if __name__ == '__main__':
     start_time = time.time()
@@ -59,12 +78,12 @@ if __name__ == '__main__':
     total_time_multi = time.time() - start_time
     print("Took {:.2f}s for multi-processed version - {:.2f} FPS".format(total_time_multi,
                                                                           t_timesteps/ total_time_multi))
-    model.save("ppo2_multi_env_cartpole")
+    # model.save("ppo2_multi_env_cartpole")
 
 
     # Single Process RL Training
-    single_process_model = PPO2(MlpPolicy, env, verbose=0)
     # single_process_model = PPO2(MlpPolicy, DummyVecEnv([lambda: gym.make(env_id)]), verbose=0)
+    single_process_model = PPO2(MlpPolicy, make_vec_env(env_id), verbose=0)
     start_time = time.time()
     single_process_model.learn(t_timesteps)
     total_time_single = time.time() - start_time
